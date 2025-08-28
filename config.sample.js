f92e57e59f93d3b309942087afd261e99183fe70
Anupong19077 <anupong.ko@saard.ac.th>
2025-08-28 09:59:10 +0700

    Create config.sample.js

---
+++ config.sample.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC_V9rsqmB8CmBnuB7UM2VaaTa3oNEZNvs",
+  authDomain: "checktime-971b6.firebaseapp.com",
+  databaseURL: "https://checktime-971b6-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "checktime-971b6",
+  storageBucket: "checktime-971b6.firebasestorage.app",
+  messagingSenderId: "387299959061",
+  appId: "1:387299959061:web:bd731a860f00772dae491c",
+  measurementId: "G-0LPLFGCW0X"
+};
+
